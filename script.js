227fcf154804db2d34360052bc5e9d684336f62c
Engr Daniel <102671504+danielnatics@users.noreply.github.com>
2022-10-06 19:53:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const sidebar = document.querySelector(".sidebar");
 const closebtn = document.getElementById("closebtn");
 const menuIcon = document.querySelector(".menu-icon");
 const aboutPage = document.querySelector(".about-page");
-const aboutButton = document.querySelector("#about-btn");
+const contactPage = document.querySelector(".contact-page");
+
 const homeButton = document.querySelector(".home");
+
+function requestPermission() {
+  Notification.requestPermission().then((perm) => {
+    alert(perm);
+  });
+}
+const not = new Notification("Hello There");
+
 menuIcon.addEventListener("click", openMenu);
 function openMenu() {
   sidebar.classList.add("translate");
@@ -12,18 +21,27 @@ closebtn.addEventListener("click", closeMenu);
 function closeMenu() {
   sidebar.classList.remove("translate");
 }
-// aboutButton.addEventListener("click", aboutOpen);
-// function aboutOpen() {
-//   alert("hi");
-// }
+
+// About section
 function openAbout() {
   aboutPage.classList.add("vision");
   sidebar.classList.remove("translate");
+  document.body.style.visibility = "hidden";
 }
-function remove() {
+function removeAbout() {
   aboutPage.classList.remove("vision");
   sidebar.classList.remove("translate");
+  document.body.style.visibility = "visible";
 }
-// homeButton.addEventListener("click", () => {
-//   aboutPage.innerHTML = "hidden";
-// });
+
+//Contact Section
+function openContact() {
+  contactPage.classList.add("vision");
+  sidebar.classList.remove("translate");
+  document.body.style.visibility = "hidden";
+}
+function removeContact() {
+  contactPage.classList.remove("vision");
+  sidebar.classList.remove("translate");
+  document.body.style.visibility = "visible";
+}
